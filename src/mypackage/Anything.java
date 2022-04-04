7771c897b27c5cc6c69e5c59977417e7c3849273
Yunho Kim <31121701+Kandreaop@users.noreply.github.com>
2022-04-04 16:15:38 +0900

    MAKE IGNORE

--- src/mypackage/Anything.java
+++ src/mypackage/Anything.java
@@ -5,8 +5,6 @@ import java.lang.Math;
 public class Anything {
 
     public static void main(String[] args) {
-        System.out.println(Math.sqrt(Math.pow((2-1),2) + Math.pow((2-1),2)));
-        System.out.println(Math.sqrt(Math.pow((2-2),2) + Math.pow((4-2),2)));
-        System.out.println(Math.sqrt(Math.pow((2-1),2) + Math.pow((4-1),2)));
+
     }
 }
